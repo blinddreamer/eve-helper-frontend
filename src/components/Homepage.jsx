fdd159f0d6e0327bb337da3e612e77041f780634
BlindDreamer <blinddreaemr@huku.rocks>
2025-03-05 13:54:36 +0200

    fix homepage some css and stuff

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -14,25 +14,24 @@ function Homepage() {
           <div>
             <Alert variant="danger">UNDER CONSTRUCTION</Alert>
           </div>
-          <main>
-            <p>
-              eve-helper is an advanced industrial calculator aiming to help
-              with your industrial costs.
-            </p>
-            <p>
+          <main style={{ lineHeight: "1.6" }}>
+            {" "}
+            {/* Adjust line height for readability */}
+            <div>
+              eve-helper is an industrial calculator aiming to help with your
+              industrial costs.
+              <br />
               Whether you're a seasoned industrialist or just starting out,{" "}
               eve-helper provides comprehensive tools and features to streamline
               your industrial operations in EVE.
-            </p>
-            <p>
+              <br />
               With eve-helper, you can calculate material requirements,
               production costs, profit margins, and more for a wide range of
-              industrial activities.{" "}
-            </p>
-            <p>
+              industrial activities.
+              <br />
               Our user-friendly interface and customizable settings make it easy
               to optimize your industry and maximize your profits.
-            </p>
+            </div>
           </main>
         </div>
       </Animated>
