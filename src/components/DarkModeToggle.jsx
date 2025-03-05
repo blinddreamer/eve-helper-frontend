b2e70647cb4d46feb03a50a9b10db70907ef9cc8
BlindDreamer <blinddreaemr@huku.rocks>
2025-03-05 13:39:10 +0200

    fix darkmode icon

--- src/components/DarkModeToggle.jsx
+++ src/components/DarkModeToggle.jsx
@@ -1,18 +1,15 @@
 import React, { useState, useEffect } from "react";
 import Button from "react-bootstrap/Button";
+import { PiSunDimLight, PiMoon } from "react-icons/pi";
 
 function DarkModeToggle() {
-  // Retrieve dark mode preference from localStorage on initial render
   const [darkMode, setDarkMode] = useState(() => {
     const storedDarkMode = localStorage.getItem("darkMode");
-    return storedDarkMode ? JSON.parse(storedDarkMode) : true; // Change default mode to true (dark mode)
+    return storedDarkMode ? JSON.parse(storedDarkMode) : true;
   });
 
   useEffect(() => {
-    // Update dark mode preference in localStorage when it changes
     localStorage.setItem("darkMode", JSON.stringify(darkMode));
-
-    // Update the data-bs-theme attribute
     document.documentElement.setAttribute(
       "data-bs-theme",
       darkMode ? "dark" : "light"
@@ -24,11 +21,21 @@ function DarkModeToggle() {
   };
 
   return (
-    <div>
-      <Button variant="secondary" onClick={toggleDarkMode}>
-        {darkMode ? "Light Mode" : "Dark Mode"}
-      </Button>
-    </div>
+    <Button
+      variant="secondary"
+      onClick={toggleDarkMode}
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        width: "3rem", // Adjust width
+        height: "3rem", // Adjust height
+        padding: "0", // Remove extra padding
+        borderRadius: "50%", // Optional: Make it a circular button
+      }}
+    >
+      {darkMode ? <PiSunDimLight size={24} /> : <PiMoon size={24} />}
+    </Button>
   );
 }
 
